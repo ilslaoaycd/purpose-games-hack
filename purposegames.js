eedf484cc2b7aa21cec99c438cbfdb512587713e
Isaac Hank Lloyd <59512775+ilslaoaycd@users.noreply.github.com>
2021-02-03 18:16:52 -0700

    Added files

---
+++ purposegames.js
@@ -0,0 +1,29 @@
+var elem = document.body;
+
+function eventFire(el, etype){
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+setInterval(function(){
+    if (elem) {
+        elem.style.outline = "none";
+        elem.style.zIndex = "unset";
+    }
+
+    elem = document.querySelectorAll('[data-text="' + document.getElementById("question-box").innerText + '"]')[1];
+
+    elem.style.outline = "1px solid red";
+    elem.style.outlineOffset = "4px";
+    elem.style.zIndex = "999999";
+    eventFire(elem, "click");
+}, 10);
